0d8c6ae28d502f749888e774da64135623854730
boshenniu <bniu@andrew.cmu.edu>
2019-09-19 16:28:05 -0400

    (1) solve dependency issue; (2) remove redundant packages

--- boshen_ws/build/catkin_generated/setup_cached.sh
+++ boshen_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/boshenniu/boshen_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/boshenniu/boshen_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/boshenniu/boshen_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/boshenniu/boshen_ws/src:$ROS_PACKAGE_PATH"
--- boshen_ws/devel/share/rosserial_msgs/cmake/rosserial_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(rosserial_msgs_MESSAGE_FILES "/home/boshenniu/boshen_ws/src/rosserial/rosserial_msgs/msg/Log.msg;/home/boshenniu/boshen_ws/src/rosserial/rosserial_msgs/msg/TopicInfo.msg")
-set(rosserial_msgs_SERVICE_FILES "/home/boshenniu/boshen_ws/src/rosserial/rosserial_msgs/srv/RequestMessageInfo.srv;/home/boshenniu/boshen_ws/src/rosserial/rosserial_msgs/srv/RequestParam.srv;/home/boshenniu/boshen_ws/src/rosserial/rosserial_msgs/srv/RequestServiceInfo.srv")
