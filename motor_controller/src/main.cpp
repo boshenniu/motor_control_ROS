b84fff2f8e183da8386e4f5b0e26e864bd8fb51e
boshenniu <bniu@andrew.cmu.edu>
2019-09-23 20:58:14 -0400

    1) finished IO API; 2) fix large difference issue

--- motor_controller/src/main.cpp
+++ motor_controller/src/main.cpp
@@ -3,36 +3,52 @@
 #include <sstream>
 
 #include "roboteq_msgs/Command.h"
+#include "roboteq_msgs/Feedback.h"
+#include "std_msgs/Float32.h"
 
-int main (int argc, char** argv) {
-  	ros::init (argc, argv, "motor_controller");
-  	ros::NodeHandle nh;
-
-    ros::Publisher desired_motor_position_pub = nh.advertise<roboteq_msgs::Command>("cmd_1",1000);
-
-    ros::Rate loop_rate(1); // 1 fps
+class MotorController {
+public:
+    double cur_pos;
+    MotorController() {
+        cmd_sub_ = n_.subscribe("command_test", 1, &MotorController::cmd_callback, this);
+        fb_sub_ = n_.subscribe("/roboteq_driver/feedback", 1, &MotorController::fb_callback, this);
+        pub_ = n_.advertise<roboteq_msgs::Command>("cmd_1",1);
+    }
     
-    int count = 0;
-    while ( ros::ok() ) {
-        std::stringstream ss;
-        ss << "hello world " << count;
-
-        ROS_INFO("%s", ss.str().c_str());
-
+    void cmd_callback(const std_msgs::Float32& cmd_in) {
         roboteq_msgs::Command command;
         command.mode = command.MODE_POSITION;
-        command.setpoint = (count % 2 == 0) ? 20 : -20;
-        // command.setpoint = 20;
 
-        // command.setpoint = count * 20;
-
-        desired_motor_position_pub.publish( command );
-        
-        ros::spinOnce();
-        loop_rate.sleep();
-        ++count;
+        // "cur_pos" and "cmd_in.data" have big difference
+        int dir = ( cmd_in.data > cur_pos ) ? 1 : -1;
+        int step = 1;
+        double temp = cur_pos;
+        while ( abs(temp - cmd_in.data) > step ) {
+            temp += dir * step;
+            command.setpoint = temp / 360. * 4096. / 20.;
+            pub_.publish( command );
+            loop_rate.sleep();
+        }
+        command.setpoint = cmd_in.data / 360. * 4096. / 20.;
+        pub_.publish( command );
     }
 
-    return 0; 
+    void fb_callback(const roboteq_msgs::Feedback& feedback) {
+        cur_pos = feedback.measured_position;
+        std::stringstream ss;
+        ss << "current position " << cur_pos << " degrees.";
+        ROS_INFO("%s", ss.str().c_str());
+    }
+    ros::NodeHandle n_;
+    ros::Subscriber cmd_sub_;
+    ros::Subscriber fb_sub_;
+    ros::Publisher pub_;
+    ros::Rate loop_rate = ros::Rate(1000);
+};
 
+int main (int argc, char** argv) {
+  	ros::init (argc, argv, "motor_controller");	
+    MotorController MCObject;
+    ros::spin();
+    return 0; 
 }
